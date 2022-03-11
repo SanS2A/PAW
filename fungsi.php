4d834360bd3b9a4bcbbb6737607cb50a0de7357d
SanS2A <sansayidus@gmail.com>
2022-03-11 10:46:20 +0700

    Menambah File fungsi.php

---
+++ fungsi.php
@@ -0,0 +1,11 @@
+<?php
+// Code Fungsi
+function hitung ($a,$b) {
+    $hasil = $a * $b;
+    return $hasil;
+}
+// Pemanggilan Fungsi
+echo "2 x 3 = ".hitung(2,3)."<br>";
+echo "3 x 4 = ".hitung(3,4)."<br>";
+echo "4 x 5 = ".hitung(4,5)
+?>
