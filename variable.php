8338e01fb90a482dec41bd98f53336c10db30b71
SanS2A <sansayidus@gmail.com>
2022-03-11 10:45:02 +0700

    Menambah file variable.php

---
+++ variable.php
@@ -0,0 +1,14 @@
+<?php
+// Variable
+$a = 3;
+$b = 4;
+$c = 5;
+// Operasi hitung
+$Jumlah = $a + $b;
+$Kurang = $b - $c;
+$kali = $c * $c;
+// Hasil
+echo"Hasil Penjumlah = $Jumlah <br>";
+echo "Hasil Pengurangan = $Kurang <br>"; 
+echo "Hasil Perkalian = $kali <br>"
+?>
